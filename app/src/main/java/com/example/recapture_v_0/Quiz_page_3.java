8caaf4f8c753c0a2a6120a616a2cd03050f55c0f
Bazif Rasool <baziflaherwal@gmail.com>
2020-02-01 12:42:55 +0530

    Quiz MCQ Integration_#1

--- app/src/main/java/com/example/recapture_v_0/Quiz_page_3.java
+++ app/src/main/java/com/example/recapture_v_0/Quiz_page_3.java
@@ -2,12 +2,15 @@ package com.example.recapture_v_0;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Quiz_page_3 extends AppCompatActivity {
 
-
+    protected int Ques_no = 0;
+    protected int score__mcq = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,18 @@ public class Quiz_page_3 extends AppCompatActivity {
     }
 
     public void McQ_button(View view) {
+        TextView ques = findViewById(R.id.textView3);
+        CheckBox op1 = findViewById(R.id.checkBox);
+        CheckBox op2 = findViewById(R.id.checkBox2);
+        CheckBox op3 = findViewById(R.id.checkBox5);
+        CheckBox op4 = findViewById(R.id.checkBox6);
+
+        if (Ques_no == 0) {
+            if (op2.isChecked() && !op1.isChecked() && !op3.isChecked() && !op4.isChecked()) ;
+            score__mcq++;
+        }
+
+
 
     }
 
